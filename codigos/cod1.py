ec83e485ccd1afc3a914ae9edf6ca138b3c1bc13
Luishernan5 <ramirezluishernan5@gmail.com>
2025-03-22 15:00:00 -0600

    cod1

---
+++ codigos/cod1.py
@@ -0,0 +1,16 @@
+class NombreClase():
+    
+    # Estructura (primero atributos luego metodos):
+    # Atributos de clase:
+    atributos1 = "valor1"
+    atributos2 = "valor2"
+
+    # Metodos:
+    def metodo_1(self):
+        print("Metodo de clase")
+    def metodo_2(self):
+        print("Metodo de clase")
+
+obj = NombreClase()
+obj.metodo_1()
+obj.metodo_2()
